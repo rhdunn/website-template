3d5649097c2ce1388b3ce247e5c4deb74cc984b3
Reece H. Dunn <msclrhd@gmail.com>
2013-04-15 17:07:51 +0100

    Properly fix parsing RDF metadata in RdfGenerator (delay parsing).

--- _plugins/rdf_generator.rb
+++ _plugins/rdf_generator.rb
@@ -4,17 +4,39 @@ require 'rdf/turtle'
 
 module Jekyll
 
+  class RdfMetadata
+    attr_accessor :prefixes, :graph
+
+    def initialize(site, page)
+      @prefixes = site.config['namespaces'].clone
+      @src = page.destination(site.dest)
+      @base_uri = File.join(site.config['url'], page.basename)
+    end
+
+    def load()
+      if @graph == nil
+        # Load the RDF data from the RDFa file.
+        @graph = RDF::Graph.load(@src, :prefixes => @prefixes, :base_uri => @base_uri)
+
+        # Remove WAI-ARIA roles from the RDF graph.
+        @graph.delete([nil, RDF::XHV.role, RDF::XHV.banner])
+        @graph.delete([nil, RDF::XHV.role, RDF::XHV.main])
+        @graph.delete([nil, RDF::XHV.role, RDF::XHV.navigation])
+      end
+    end
+  end
+
   class RdfFile < StaticFile
-    def initialize(site, base, dir, name, graph, prefixes)
+    def initialize(site, base, dir, name, metadata)
       super(site, base, dir, name)
-      @graph = graph
-      @prefixes = prefixes
+      @metadata = metadata
     end
 
     def write(dest)
       dest_path = destination(dest)
-      RDF::Writer.open(dest_path, :prefixes => @prefixes) do |writer|
-        @graph.each_statement do |statement|
+      @metadata.load()
+      RDF::Writer.open(dest_path, :prefixes => @metadata.prefixes) do |writer|
+        @metadata.graph.each_statement do |statement|
           writer << statement
         end
       end
@@ -23,7 +45,6 @@ module Jekyll
 
   class RdfGenerator < Generator
     safe true
-    priority :low
 
     EXTENSIONS = {
       'rdfxml' => '.rdf'
@@ -32,23 +53,10 @@ module Jekyll
     def generate(site)
       site.pages.each do |page|
         if page.output_ext == '.html' and page.data['rdf']
-          page.write(site.dest)
-
-          prefixes = site.config['namespaces'].clone
-          src = page.destination(site.dest)
-          base_uri = File.join(site.config['url'], page.basename)
-
-          # Load the RDF data from the RDFa file.
-          graph = RDF::Graph.load(src, :prefixes => prefixes, :base_uri => base_uri)
-
-          # Remove WAI-ARIA roles from the RDF graph.
-          graph.delete([nil, RDF::XHV.role, RDF::XHV.banner])
-          graph.delete([nil, RDF::XHV.role, RDF::XHV.main])
-          graph.delete([nil, RDF::XHV.role, RDF::XHV.navigation])
-
+          metadata = RdfMetadata.new(site, page)
           page.data['rdf'].each do |type|
             filename = page.url.clone.sub('.html', EXTENSIONS[type])
-            site.static_files << RdfFile.new(site, site.dest, '/', filename, graph, prefixes)
+            site.static_files << RdfFile.new(site, site.dest, '/', filename, metadata)
           end
         end
       end
