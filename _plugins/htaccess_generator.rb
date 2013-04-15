61174758c641ee2188043c8826fe9ec3a3119c61
Reece H. Dunn <msclrhd@gmail.com>
2013-04-15 23:20:56 +0100

    Simplify the htaccess file generation.

--- _plugins/htaccess_generator.rb
+++ _plugins/htaccess_generator.rb
@@ -2,55 +2,28 @@ module Jekyll
 
   class HtAccessFile < StaticFile
     def write(dest)
-      metadata = []
-      @site.pages.each do |p|
-        if p.output_ext == '.html' and p.data['rdf']
-          rdf = p.url.clone.sub('.html', '.rdf')
-          url = p.basename.clone.sub('$\/', '')
-          metadata << { 'canonical' => url , 'html' => p.url , 'rdf' => rdf }
-        end
-      end
-
       dest_path = destination(dest)
       File.open(dest_path, 'w') do |f|
-        if @site.config['indices']
-          f.write("# Enable directory listing\n")
-          f.write("Options +Indexes\n")
-        else
-          f.write("# Disable directory listing\n")
-          f.write("Options -Indexes\n")
-        end
-        f.write("\n")
-        f.write("# Disable multiviews\n")
         f.write("Options -MultiViews\n")
+        f.write("Options -Indexes\n")
         f.write("\n")
-        f.write("# Custom mimetypes\n")
         f.write("AddType application/rdf+xml .rdf\n")
         f.write("\n")
-        f.write("# Enable mod_rewrite\n")
         f.write("RewriteEngine On\n")
         f.write("\n")
-        if !metadata.empty?
-          f.write("# Rewrite rule to serve HTML content from the vocabulary URI if requested\n")
-          f.write("RewriteCond %{HTTP_ACCEPT} !application/rdf\+xml.*(text/html|application/xhtml\+xml)\n")
-          f.write("RewriteCond %{HTTP_ACCEPT} text/html [OR]\n")
-          f.write("RewriteCond %{HTTP_ACCEPT} application/xhtml\+xml [OR]\n")
-          f.write("RewriteCond %{HTTP_USER_AGENT} ^Mozilla/.*\n")
-          metadata.each do |item|
-            f.write("RewriteRule ^#{item['canonical']}$ #{item['html']} [R=303]\n")
-          end
-          f.write("\n")
-          f.write("# Rewrite rule to serve RDF/XML content from the vocabulary URI if requested\n")
-          f.write("RewriteCond %{HTTP_ACCEPT} application/rdf\+xml\n")
-          metadata.each do |item|
-            f.write("RewriteRule ^#{item['canonical']}$ #{item['rdf']} [R=303]\n")
-          end
-          f.write("\n")
-          f.write("# Rewrite rule to serve the RDF/XML content from the vocabulary URI by default\n")
-          metadata.each do |item|
-            f.write("RewriteRule ^#{item['canonical']}$ #{item['rdf']} [R=303]\n")
-          end
-        end
+        f.write("# Redirect X.html to X\n")
+        f.write("RewriteCond %{THE_REQUEST} ^(GET|HEAD)\\ /.+\\.html\\ HTTP\n")
+        f.write("RewriteRule ^(.+)\\.html$ http://%{HTTP_HOST}/$1 [R=301,L]\n")
+        f.write("\n")
+        f.write("# If RDF/XML is requested, try X.rdf\n")
+        f.write("RewriteCond %{HTTP_ACCEPT} application/rdf\\+xml\n")
+        f.write("RewriteCond %{REQUEST_FILENAME}.rdf -f\n")
+        f.write("RewriteRule ^.+$ %{REQUEST_FILENAME}.rdf [L]\n")
+        f.write("\n")
+        f.write("# If X does not exist, try X.html\n")
+        f.write("RewriteCond %{REQUEST_FILENAME} !-f\n")
+        f.write("RewriteCond %{REQUEST_FILENAME}.html -f\n")
+        f.write("RewriteRule ^.+$ %{REQUEST_FILENAME}.html [L]\n")
       end
     end
   end
